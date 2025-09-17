c3b7f0b152fa6ea9aceac6f4ab6f58f4dcf90b96
HarryLinn <harrylinn014@gmail.com>
2025-09-17 13:20:03 -0400

    work

--- src/DogWalkCompany.java
+++ src/DogWalkCompany.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class DogWalkCompany {
+    private int[] schedule;
+
+    public DogWalkCompany() {
+        schedule = new int[24];
+    }
+
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < schedule.length; i++)
+            s += i + " " + schedule[i] + "\n";
+        return s;
+    }
 }
+
